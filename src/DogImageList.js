38c745bba9851ad0cbd2771b73b9f9abfb8ce512
marave <marek.vaskovych@seznam.cz>
2024-09-16 16:08:27 +0200

    Button finally work, remains finished app in css

--- src/DogImageList.js
+++ src/DogImageList.js
@@ -1,31 +1,42 @@
 
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./DogImageList.css";
 
 
 function DogImageList() {
-    const [image, setImage] = useState('');
+    const [image, setImage] = useState(null);
+
 
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch('https://dog.ceo/api/breeds/image/random');
-                const data = await response.json();
-                setImage(data.message);
-            } catch (error) {
-                console.error('Nepodařilo se načíst obrázek', error);
-            }
-        };
-        fetchData();
+        fetchImage();
     }, []);
+
+    const fetchImage = async () => {
+        try {
+            const response = await fetch('https://dog.ceo/api/breeds/image/random');
+            const data = await response.json();
+            setImage(data.message);
+        } catch (error) {
+            console.error('Nepodařilo se načíst obrázek', error);
+        }
+
+    };
+
+    const handleRefreshButton = () => {
+        fetchImage();
+    };
+
     return (
         <div className="card" style={{ width: "30rem", height: "45rem" }}>
             <div className="text-center">
                 <h1 className="display-6">Random dog breed</h1>
-                <img className="card-img-top" src={image} alt="You can see random dogs" />
-                <button className="btn btn-success">Click for another dog!</button>
+                <div>
+                    {image && <img className="card-img-top" src={image} alt="You can see random dogs" />}
+                    <button className="btn btn-success" onClick={handleRefreshButton}>Click for another dog!</button>
+                </div>
+
             </div>
 
         </div>
