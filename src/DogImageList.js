8cadd5eaee69f04c85fef54625c961eaa481d814
marave <marek.vaskovych@seznam.cz>
2024-09-16 13:12:20 +0200

    I add button, it needs working

--- src/DogImageList.js
+++ src/DogImageList.js
@@ -1,17 +1,19 @@
 
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 
 function DogImageList() {
-    const [breed, setBreed] = useState('');
+    const [image, setImage] = useState('');
+
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const response = await fetch('https://dog.ceo/api/breeds/image/random');
                 const data = await response.json();
-                setBreed(data.message);
+                setImage(data.message);
             } catch (error) {
                 console.error('Nepodařilo se načíst obrázek', error);
             }
@@ -19,9 +21,13 @@ function DogImageList() {
         fetchData();
     }, []);
     return (
-        <div>
-            <h1>Random dog breed</h1>
-            <img src={breed} alt="You can see random dogs" />
+        <div className="card" style={{ width: "30rem", height: "45rem" }}>
+            <div className="text-center">
+                <h1 className="display-6">Random dog breed</h1>
+                <img className="card-img-top" src={image} alt="You can see random dogs" />
+                <button className="btn btn-success">Click for another dog!</button>
+            </div>
+
         </div>
     );
 };
